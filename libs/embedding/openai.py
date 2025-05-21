15a70182b77cb3ba5d1c90b53639404780bf777f
Marco Caimi <mcaimi@redhat.com>
2025-05-21 14:45:38 +0200

    Fix constructor parameter in OpenAIEmbeddingModel

--- libs/embedding/openai.py
+++ libs/embedding/openai.py
@@ -5,4 +5,4 @@ from llama_index.embeddings.openai import OpenAIEmbedding
 
 
 def openai_instance(base_url="http://localhost:11434", model="llama2:7b", api_key=None) -> Callable:
-    return OpenAIEmbedding(api_base=base_url, model=model, api_key=api_key)
+    return OpenAIEmbedding(api_base=base_url, model_name=model, api_key=api_key)
